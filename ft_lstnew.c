707caa1b848e702d1a7f31b67c21196f3edc5295
Alejandro Luis Acosta <aacosta@c1r2s1.42madrid.com>
2024-02-07 14:53:52 +0100

    ft_lstnew added - 02/07

---
+++ ft_lstnew.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aacosta <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 14:36:03 by aacosta           #+#    #+#             */
+/*   Updated: 2024/02/07 14:52:09 by aacosta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = malloc(1 * sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/*int	main(void)
+{
+	t_list *new_node = ft_lstnew((void *)NULL);
+	printf("new node's content: %d\n", (int)new_node->content);
+	printf("new node's 'next': %p\n", new_node->next);
+}*/
